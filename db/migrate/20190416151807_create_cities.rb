5e2ae36ae8f5aee4a79974c6e3ef5c8c64dda4ae
dnj547 <dnj547@live.com>
2019-04-16 11:27:32 -0400

    Create tables

--- db/migrate/20190416151807_create_cities.rb
+++ db/migrate/20190416151807_create_cities.rb
@@ -1,4 +1,10 @@
 class CreateCities < ActiveRecord::Migration[5.0]
   def change
+    create_table :cities do |t|
+      t.string :name
+      t.string :location
+      t.integer :population
+      t.string :time_zone
+    end
   end
 end
