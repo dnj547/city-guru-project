dcb42ef85e91854ac31bd7d856fd91a6b5e236b8
axionos <axionoso@gmail.com>
2019-04-16 12:00:16 -0400

    Add seed data and require app folder in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require 'rest-client'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+require_all 'app'
 require_all 'lib'
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+sf = City.find_or_create_by(name: "San Francisco", location: 'CA, US', population: 500000, time_zone: "PST")
