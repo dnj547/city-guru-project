a6d159b53980b0f6ef915141b3f4b0a00aa79934
dnj547 <dnj547@live.com>
2019-04-16 12:51:00 -0400

    Add has many, belongs to relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 sf = City.find_or_create_by(name: "San Francisco", location: 'CA, US', population: 500000, time_zone: "PST")
+danielle = User.find_or_create_by(name: "Danielle")
+soyeong = User.find_or_create_by(name: "Soyeong")
