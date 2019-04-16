3cb7313887e17b552c976ba2d38a2216eb40d507
dnj547 <dnj547@live.com>
2019-04-16 16:23:09 -0400

    Remove valid_city? method because it is included in city_info method

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,6 +5,7 @@ require 'pry'
 def city_info(city_name)
 
   # showing list of cities and searching through them for the city name
+  # search function is smart enough to find whichever city is closest to the city name entered
   url = 'https://api.teleport.org/api/cities/?search=' + city_name
   response_string = RestClient.get(url)
 
@@ -18,23 +19,5 @@ def city_info(city_name)
   new_response_string = RestClient.get(city_url)
   new_response_hash = JSON.parse(new_response_string)
   puts new_response_hash
+  binding.pry
 end
-
-def valid_city?(city_name)
-
-  # Below api returns only 25 cities out of all the cities
-  # so we are going to look at the teleport.org/cities
-  # to see all the cities
-
-  # url = 'https://api.teleport.org/api/cities/'
-  # response_string = RestClient.get(url)
-  # response_hash = JSON.parse(response_string)
-  # city_data = response_hash["_embedded"]["city:search-results"].map do |city_hash|
-  #   city_hash["matching_full_name"].split(",")[0].downcase
-  # end
-  #
-  # city_data.include?(city_name.downcase)
-
-  
-end
-puts valid_city?("Sydney")
