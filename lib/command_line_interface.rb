7b2e80413fdf9f1e374d36e32f5a754e8779067c
axionos <axionoso@gmail.com>
2019-04-17 09:48:20 -0400

    Refactor main_menu method and defined main_menu_options method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -32,44 +32,47 @@ def user_exists?(user_name)
   !User.find_by(name: user_name).nil?
 end
 
-def output_if_user_exists(user_name)
-  puts "Welcome back, #{user_name}! What would you like to do? Please enter a number."
-  puts "1) See my favorite cities"
-  puts "2) Search for a city"
-  puts "3) Fun facts"
-  puts "4) Exit"
+
+###################### MENU METHODS ######################
+
+def welcome_message(user_name)
+  if user_exists?(user_name)
+    puts "Welcome back to City Guru, #{user_name}! What would you like to do? Please enter a number."
+  else
+    puts "Hi, #{user_name}. This is City Guru. What would you like to do? Please enter a number."
+  end
+  puts "(Type E to exit)"
+  puts "1. Look at favorites"
+  puts "2. Search for a city"
+  puts "3. Fun facts"
 end
 
-def output_if_user_does_not_exist(user_name)
-  puts "Hi, #{user_name}. This is City Guru. What would you like to do? Please enter a number."
-  puts "1) Search for a city"
-  puts "2) Fun facts"
-  puts "3) Exit"
-end
 
-def welcome_menu_user_exist(selection)
-  if selection == 1
 
-  elsif selection == 2
+def main_menu_options(user_name, input)
+  welcome_message(user_name)
+  if input == 1
+    # move to favorites
+  elsif input == 2
+    # move to search for city
+  elsif input == 3
+    # move to fun facts
+  elsif input.downcase == 'e'
+    exit(input)
+  elsif input.downcase == 'm'
+    welcome_message(user_name)
+  else
+    puts "Invalid input. Please select a number from the menu."
+    welcome_message(user_name)
+  end
 
-  elsif selection == 3
 
-  elsif
-    selection == 4
+  def exit
+    puts "Good bye and see you again!"
     exit!
   end
 end
 
-def welcome_menu_new_user(selection)
-  if selection == 1
-
-  elsif selection == 2
-
-  elsif
-    selection == 3
-    exit!
-  end
-end
 
 def city_info_menu(selection)
   if selection == 1
@@ -88,4 +91,4 @@ def salary_data_menu(selection)
 
 end
 
-###################### HELPER METHODS ######################
+###################### MENU METHODS ######################
